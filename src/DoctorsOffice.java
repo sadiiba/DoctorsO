eae2785551e1cd4b8429bd0519f4d765e99152d5
sadiiba <sadiiba@gmail.com>
2020-06-17 00:22:57 -0400

    DoctorsO

---
+++ src/DoctorsOffice.java
@@ -0,0 +1,16 @@
+public class DoctorsOffice {
+
+    public static void main(String[] args) {
+
+        String patientName= "Stacey Manson";
+        boolean hasInsurance= false;
+        double temp= 102.2;
+        int numVisits= 5;
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: " +patientName);
+        System.out.println("Patient has insurance: " +hasInsurance);
+        System.out.println("Body temperature:" + temp);
+        System.out.println("Number of Visits:" + numVisits);
+    }
+    }
